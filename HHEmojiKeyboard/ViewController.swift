58c65a8e1afa7daad8a6885c48b0f44d5ab70dbd
517566879@qq.com <xoxo@xoxodemacbook-pro.local>
2016-06-06 09:52:30 +0800

    Initial Commit

---
+++ HHEmojiKeyboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HHEmojiKeyboard
+//
+//  Created by xoxo on 16/6/6.
+//  Copyright © 2016年 caohuihui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
